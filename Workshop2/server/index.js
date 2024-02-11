00aa4e3369586b3facf84ecc29dc4f51986bc884
Cristopher <cristopheralvarado101@gmail.com>
2024-02-10 22:11:15 -0600

    Implemented a client-server app allowing users to request exchange rates in USD and EUR for all countries. Utilize two server-side APIs for data retrieval.

---
+++ Workshop2/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const routes = require('./routes/routes');
+
+const app = express();
+
+//middlewares
+app.use(bodyParser.json());
+app.use(cors({
+  domains: '*',
+  methods: "*"
+}));
+
+//routes
+app.use('/', routes); // Use the file of routes
+
+//start the app
+app.listen(3001, () => console.log(`BBCR Exchange type service listening on port 3001!`));
---
+++ Workshop2/server/routes/routes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+
+// Endpoints
+
+// Get the exchange data in USD and EUR based on the currency type.
+router.get('/tipocambio', async (req, res) => {
+    try {
+        let currencyType = req.query.currency.toLowerCase();
+
+        const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currencyType}.json`);
+        const data = await response.json();
+
+        const exchangeRates = {
+            currency: currencyType,
+            exchangeRateUSD: data[Object.keys(data)[1]].usd,
+            exchangeRateEUR: data[Object.keys(data)[1]].eur
+        }
+        res.json(exchangeRates);
+    }
+    catch (error) {
+        console.error('Error al solicitar la tipo de cambio:', error);
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// Get all countries and related information
+router.get('/paises', async (req, res) => {
+    try {
+        const response = await fetch('https://restcountries.com/v3.1/all'); // Solicitud a la API de tercero
+        const data = await response.json();
+
+        const allCountries = data.map(country => ({
+            name: country.name.common,
+            currency: country.currencies ? Object.keys(country.currencies)[0] : null
+        }));
+
+        res.json(allCountries);
+    } catch (error) {
+        console.error('Error al solicitar la información de los países:', error);
+        res.status(400).json({ message: error.message });
+    }
+});
+
+module.exports = router;
